c7bd1b743e6e785c6c1b982eeb0e5580242f4a81
zhangheyang <36380047@qq.com>
2020-01-12 15:00:14 +0800

    烧写地址需与boot.bin中的对应，windows下改了，Linux也提交一下。

--- rt-thread/script/linux/write_spiflash.sh
+++ rt-thread/script/linux/write_spiflash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-sudo ../tools/sunxi-tools/sunxi-fel -p spiflash-write 0x00000000 ../f1c100s_spl/boot.bin
-sudo ../tools/sunxi-tools/sunxi-fel -p spiflash-write 0x00100000 rtthread.bin
+../tools/sunxi-tools/sunxi-fel -p spiflash-write 0x00000000 ../f1c100s_spl/boot.bin
+../tools/sunxi-tools/sunxi-fel -p spiflash-write 0x00010000 rtthread.bin
